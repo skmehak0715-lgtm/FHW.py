61e88840b944707fb62e1c05132a841e95e0a9d8
skmehak0715-lgtm <sk.mehak0715@gmail.com>
2026-05-22 17:40:04 +0530

    Add files via upload

---
+++ FHW1.py
@@ -0,0 +1,33 @@
+print("my Daily routine")
+
+# step 1: Waking up
+print("I wake up at 8 AM.")
+
+# step 2: Going to school
+print("I go shool at 9 AM.")
+
+# step 3: Playing after school
+print("I play with my friends in the evening.")
+
+# step 4: Studying
+print("I study for 2 hours at night.")
+
+print("total study hours in a week :", 2 * 7)
+
+# Printing the title
+print("My Daily Routine")
+
+# step 1 : Waking up
+print("I wake up at 8 AM.")
+
+# step 2 : Going to school
+print("I go to school at 9 AM.")
+
+# step 3 : palying after school
+print("I play with my friends in the evening.")
+
+# step 4 : Studying
+print("I study for 2 hours at night.")
+
+# Calculating total study time in a week
+print("Total study hours in a week:", 2 * 7)
